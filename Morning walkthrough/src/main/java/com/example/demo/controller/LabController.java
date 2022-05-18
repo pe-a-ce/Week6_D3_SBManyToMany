1e9be189d200a393c193fddb53f7f0c30fb96f70
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-05-18 14:00:30 +0100

    edited application.properties - added hibernate logs and jdbc url

--- Morning walkthrough/src/main/java/com/example/demo/controller/LabController.java
+++ Morning walkthrough/src/main/java/com/example/demo/controller/LabController.java
@@ -1,11 +1,38 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Lab;
 import com.example.demo.repository.LabRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LabController {
+    // The right way to inject dependencies
+    private final LabRepository labRepository;
 
+    public LabController(LabRepository labRepository) {
+        this.labRepository = labRepository;
+    }
+
+//        @GetMapping("/labs")
+//    public ResponseEntity<List<Lab>> getAllLabs(){
+//            List<Lab> labs = labRepository.findAll();
+//            return ResponseEntity
+//                    .ok()
+//                    .body(labs);
+//        }
+//
+//    @PostMapping("/labs")
+//    public ResponseEntity<Lab> createLab(@RequestBody Lab lab) {
+//        Lab result = labRepository.save(lab);
+//        return ResponseEntity
+//                .ok() .body(result);
+//    }
 
 
 }
--- Morning walkthrough/src/main/java/com/example/demo/controller/StudentController.java
+++ Morning walkthrough/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,4 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StudentController {
-}
+
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllLabs(){
+        List<Student> students = studentRepository.findAll();
+        return ResponseEntity .ok()
+                .body(students);
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<Student> createLab(@RequestBody Student student) {
+
+
+    Student result = studentRepository.save(student);
+return ResponseEntity
+        .ok() .body(result);
+
+}}
